6cc68b9ea01a556ee61e0fa80d4544f38152b0ad
amankolhe <amankolhe3@gmail.com>
2025-06-11 16:09:14 +0530

    updTWS

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ let users = [
     { name: "virat", email: "virat@gmail.com" },
     { name: "dhoni", email: "dhoni@gmail.com" },
     { name: "deepak", email: "deepak@gmail.com" },
+     { name: "aman", email: "aman@gmail.com" },
+
 ];
 app.get("/api/users", (req, res) => {
     res.status(200).json({
